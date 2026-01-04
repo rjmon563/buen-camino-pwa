b7094a7cedbcebdae55b8bff4d654aa237b701e1
rjmon563 <rjmon563@gmail.com>
2026-01-04 16:17:24 +0100

    Fix: Manual tailwind and postcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,26 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      borderRadius: {
-        '4xl': '2rem',
-        '5xl': '3rem',
-      },
-      animation: {
-        'stamp': 'stamp-effect 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275) forwards',
-      },
-      colors: {
-        camino: {
-          blue: '#2563eb',
-          amber: '#d97706',
-          slate: '#f8fafc',
-        }
-      },
-      boxShadow: {
-        'soft': '0 20px 50px rgba(0, 0, 0, 0.05)',
-        'sos': '0 20px 50px rgba(220, 38, 38, 0.3)',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
